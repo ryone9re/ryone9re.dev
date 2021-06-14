fd461243f4bab25bc6a5b6b52c4cd32a1ea7e375
KuRImoN0o6 <ryoichi.kurimoto@gmail.com>
2021-06-15 03:31:00 +0900

    [add] Add header

---
+++ components/Header.vue
@@ -0,0 +1,23 @@
+<template>
+  <header>
+    <a href="/"><h1>ryone9re</h1></a>
+  </header>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style scoped>
+header h1 {
+  text-align: center;
+  font-weight: 300;
+  font-size: calc(10px + 3vw);
+  color: #8697a6;
+  letter-spacing: 1px;
+  margin: 0;
+}
+header a {
+  text-decoration: none;
+}
+</style>
