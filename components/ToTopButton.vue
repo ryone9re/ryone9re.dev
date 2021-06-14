7c313d277c9ba25679ca68417dab51ba2259809c
KuRImoN0o6 <ryoichi.kurimoto@gmail.com>
2021-06-15 03:33:53 +0900

    [add] Create to top button

---
+++ components/ToTopButton.vue
@@ -0,0 +1,17 @@
+<template>
+  <nuxt-link to="/" class="button--top"
+    ><font-awesome-icon :icon="['fas', 'times']"
+  /></nuxt-link>
+</template>
+
+<style>
+.button--top {
+  margin: 2%;
+  color: #8697a6;
+  font-size: calc(10px + 2vw);
+}
+
+.button--top:hover {
+  color: #bfcdd9;
+}
+</style>
