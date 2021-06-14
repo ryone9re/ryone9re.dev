e3623abdb139d23cf5decd96e314001ebe7eee0e
KuRImoN0o6 <ryoichi.kurimoto@gmail.com>
2021-06-15 03:37:33 +0900

    [update] Change design on mobile devices

---
+++ pages/index.vue
@@ -0,0 +1,31 @@
+<template>
+  <TopContent />
+</template>
+
+<script>
+export default {}
+</script>
+
+<style>
+HTML,
+BODY,
+#__nuxt,
+#__layout {
+  margin: 0;
+  width: 100%;
+  height: 100%;
+}
+
+#__layout {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+
+HTML {
+  background: #edede8;
+  background-size: cover;
+  background-position: 50% 50%;
+  height: 100%;
+}
+</style>
