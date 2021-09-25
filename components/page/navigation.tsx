adee96e192dcfa34e4c9a57a8a226091c111f66e
ryone9re <ryoichi.kurimoto@gmail.com>
2021-09-25 19:56:19 +0900

    Delete waste space

--- components/page/navigation.tsx
+++ components/page/navigation.tsx
@@ -6,11 +6,6 @@ export const Navigation = (): JSX.Element => {
   controls.stop()
   return (
     <nav id='header' className='fixed w-full z-10 top-0 bg-white'>
-      <div
-        id='progress'
-        className='h-1 z-20 top-0'
-        style={{ background: 'linear-gradient(to right, #4dc0b5 var(--scroll), transparent 0)' }}
-      />
       <div className='w-full md:max-w-4xl mx-auto flex flex-wrap items-center justify-between mt-0 py-3'>
         <div className='pl-4'>
           <Link href='/'>
