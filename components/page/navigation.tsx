a7628803c6df9a9a1a0248f83264b50e01402fa0
ryone9re <ryoichi.kurimoto@gmail.com>
2021-09-25 19:43:53 +0900

    Add home button

--- components/page/navigation.tsx
+++ components/page/navigation.tsx
@@ -1,6 +1,9 @@
+import { animationControls } from 'framer-motion'
 import Link from 'next/link'
 
 export const Navigation = (): JSX.Element => {
+  const controls = animationControls()
+  controls.stop()
   return (
     <nav id='header' className='fixed w-full z-10 top-0 bg-white'>
       <div
@@ -16,20 +19,24 @@ export const Navigation = (): JSX.Element => {
             </a>
           </Link>
         </div>
-        <div className='invisible block lg:hidden pr-4'>
-          <button
-            id='nav-toggle'
-            className='flex items-center px-3 py-2 border rounded text-gray-500 border-gray-600 hover:text-gray-900 hover:border-green-500 appearance-none focus:outline-none'
-          >
-            <svg
-              className='fill-current h-3 w-3'
-              viewBox='0 0 20 20'
-              xmlns='http://www.w3.org/2000/svg'
-            >
-              <title>Menu</title>
-              <path d='M0 3h20v2H0V3zm0 6h20v2H0V9zm0 6h20v2H0v-2z' />
-            </svg>
-          </button>
+        <div className='block lg:hidden pr-4'>
+          <Link href='/'>
+            <a>
+              <button
+                id='nav-toggle'
+                className='flex items-center px-3 py-2 border rounded text-gray-500 border-gray-600 hover:text-gray-900 hover:border-green-500 appearance-none focus:outline-none'
+              >
+                <svg
+                  className='fill-current h-3 w-3'
+                  viewBox='0 0 576 512'
+                  xmlns='http://www.w3.org/2000/svg'
+                >
+                  <title>Home</title>
+                  <path d='M280.37 148.26L96 300.11V464a16 16 0 0 0 16 16l112.06-.29a16 16 0 0 0 15.92-16V368a16 16 0 0 1 16-16h64a16 16 0 0 1 16 16v95.64a16 16 0 0 0 16 16.05L464 480a16 16 0 0 0 16-16V300L295.67 148.26a12.19 12.19 0 0 0-15.3 0zM571.6 251.47L488 182.56V44.05a12 12 0 0 0-12-12h-56a12 12 0 0 0-12 12v72.61L318.47 43a48 48 0 0 0-61 0L4.34 251.47a12 12 0 0 0-1.6 16.9l25.5 31A12 12 0 0 0 45.15 301l235.22-193.74a12.19 12.19 0 0 1 15.3 0L530.9 301a12 12 0 0 0 16.9-1.6l25.5-31a12 12 0 0 0-1.7-16.93z' />
+                </svg>
+              </button>
+            </a>
+          </Link>
         </div>
         <div
           className='w-full flex-grow lg:flex lg:items-center lg:w-auto hidden mt-2 lg:mt-0 bg-gray-100 md:bg-transparent z-20'
@@ -37,9 +44,9 @@ export const Navigation = (): JSX.Element => {
         >
           <ul className='list-reset lg:flex justify-end flex-1 items-center'>
             <li className='mr-3'>
-              <Link href='/blog/page/1'>
+              <Link href='/'>
                 <a className='inline-block text-gray-600 no-underline hover:text-gray-900 hover:text-underline py-2 px-4'>
-                  BlogTop
+                  Home
                 </a>
               </Link>
             </li>
@@ -47,6 +54,13 @@ export const Navigation = (): JSX.Element => {
               <Link href='/about'>
                 <a className='inline-block text-gray-600 no-underline hover:text-gray-900 hover:text-underline py-2 px-4'>
                   About
+                </a>
+              </Link>
+            </li>
+            <li className='mr-3'>
+              <Link href='/blog/page/1'>
+                <a className='inline-block text-gray-600 no-underline hover:text-gray-900 hover:text-underline py-2 px-4'>
+                  Blog
                 </a>
               </Link>
             </li>
