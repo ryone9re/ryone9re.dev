f3b3d8b5c718ed8e22c77a9d6620b97ea2497665
ryone9re <ryoichi.kurimoto@gmail.com>
2023-05-08 20:02:23 +0900

    山3

--- src/components/models/Mountain.tsx
+++ src/components/models/Mountain.tsx
@@ -2,9 +2,33 @@ import { OrbitControls } from '@react-three/drei';
 import { Canvas, MeshProps } from '@react-three/fiber';
 import { motion } from 'framer-motion-3d';
 import { useRef } from 'react';
-import { MeshBasicMaterial, Shape, ShapeGeometry, Vector2 } from 'three';
+import {
+  BufferGeometry,
+  Color,
+  Float32BufferAttribute,
+  MeshBasicMaterial,
+  Shape,
+  ShapeGeometry,
+  Vector2
+} from 'three';
 
-const Mount1 = () => {
+function generateVertexColors(geometry: BufferGeometry, baseColor: number) {
+  const positionAttribute = geometry.getAttribute('position');
+  const colors = [];
+  const color = new Color(baseColor);
+
+  for (let i = 0; i < positionAttribute.count; i++) {
+    const y = positionAttribute.getY(i);
+    const darkness = 1 - y / 20;
+    color.set(baseColor).multiplyScalar(darkness);
+    colors.push(color.r, color.g, color.b);
+  }
+
+  const colorAttribute = new Float32BufferAttribute(colors, 3);
+  geometry.setAttribute('color', colorAttribute);
+}
+
+function Mount1() {
   const meshRef = useRef<MeshProps>(null);
 
   const shape = new Shape([
@@ -12,7 +36,7 @@ const Mount1 = () => {
     new Vector2(-11, 19),
     new Vector2(-5, 16),
     new Vector2(0, 18),
-    new Vector2(7, 12),
+    new Vector2(7, 14),
     new Vector2(13, 15),
     new Vector2(30, 0)
   ]);
@@ -21,9 +45,9 @@ const Mount1 = () => {
   const material = new MeshBasicMaterial({ color: 0x6272a4 });
 
   return <motion.mesh ref={meshRef} geometry={geometry} material={material} />;
-};
+}
 
-const Mount2 = () => {
+function Mount2() {
   const meshRef = useRef<MeshProps>(null);
 
   const shape = new Shape([
@@ -31,7 +55,7 @@ const Mount2 = () => {
     new Vector2(-10, 12),
     new Vector2(-5, 10.5),
     new Vector2(2.5, 15.5),
-    new Vector2(8, 12),
+    new Vector2(8, 13),
     new Vector2(13, 14),
     new Vector2(30, 0)
   ]);
@@ -40,7 +64,7 @@ const Mount2 = () => {
   const material = new MeshBasicMaterial({ color: 0x44475a });
 
   return <motion.mesh ref={meshRef} geometry={geometry} material={material} />;
-};
+}
 
 export function Mountains() {
   return (
