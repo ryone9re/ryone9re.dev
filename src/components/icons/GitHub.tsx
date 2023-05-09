db8deb611b844f606501729dfa6c4b621cd491ce
ryone9re <48243891+ryone9re@users.noreply.github.com>
2023-05-10 05:52:16 +0900

    アイコンが微妙に見切れてるのを修正

--- src/components/icons/GitHub.tsx
+++ src/components/icons/GitHub.tsx
@@ -7,7 +7,7 @@ export const GitHub = () => {
             xmlns='http://www.w3.org/2000/svg'
             className='h-6 w-6'
             fill='none'
-            viewBox='0 0 98 98'
+            viewBox='-1 -1 98 98'
             stroke='currentColor'
           >
             <path
